6c1c878ca8a15cb093d378484056d6a0952ea3ef
kjw0615 <kjw0615@gmail.com>
2024-01-08 16:14:59 +0900

    kjw

--- config/settings.py
+++ config/settings.py
@@ -74,16 +74,21 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "python_db",
+#         "HOST" : "localhost",
+#         "PORT" : "3306",
+#         "USER" : "maria",
+#         "PASSWORD": "1234",              
+# }
+# }
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "python_db",
-        "HOST" : "localhost",
-        "PORT" : "3306",
-        "USER" : "maria",
-        "PASSWORD": "1234",              
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite",
 }
-
 }
 
 
